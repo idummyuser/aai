0aa803f9742e933b08cad7d52445a04a78f31eb9
idummyuser <123351202+idummyuser@users.noreply.github.com>
2023-01-23 21:10:58 +0530

    Add files via upload

---
+++ prac-8a.py
@@ -0,0 +1,21 @@
+#Write an application to implement BFS and DFS algorithm.
+#BFS algorithm
+import collections
+def bfs(graph, root):
+
+    visited, queue = set(), collections.deque([root])
+    visited.add(root)
+
+    while queue:
+        vertex = queue.popleft()
+        print(str(vertex) + " ", end="")
+
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+if __name__ == '__main__':
+    graph = {0: [1,2], 1: [2], 2: [3], 3: [1, 2]}
+    print("Follwing is Breadth First Traversal: ")
+    bfs(graph, 0)
